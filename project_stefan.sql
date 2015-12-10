44387f29bd5f947b963628ba6b735273e258641d
stmi11 <stefan.milakovic92@gmail.com>
2015-12-10 16:57:16 +0100

    Database update
    The latest version of the database, which has integrated the additions
    made by Anif/Hani and Stefan

--- project_stefan.sql
+++ project_stefan.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Värd: 127.0.0.1
--- Tid vid skapande: 01 dec 2015 kl 22:29
+-- Tid vid skapande: 10 dec 2015 kl 16:54
 -- Serverversion: 5.6.17
 -- PHP-version: 5.5.12
 
@@ -32,18 +32,7 @@ CREATE TABLE IF NOT EXISTS `courses` (
   `description` varchar(250) COLLATE latin1_general_ci NOT NULL,
   `code` varchar(45) COLLATE latin1_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=12 ;
-
---
--- Dumpning av Data i tabell `courses`
---
-
-INSERT INTO `courses` (`id`, `name`, `description`, `code`) VALUES
-(5, 'project', 'descr', 'pa1415'),
-(8, 'at1213', 'cool course', 'the good course'),
-(9, 'new project course', 'Very cool course with SImon Pudding', 'pa1112'),
-(10, 'asd', 'das ', 'pa1113'),
-(11, 'The real deal: How to conduct business', 'This course is being done by Donald Trump, bless his heart', 'pa9090');
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=41 ;
 
 -- --------------------------------------------------------
 
@@ -56,7 +45,7 @@ CREATE TABLE IF NOT EXISTS `slides` (
   `title` varchar(45) COLLATE latin1_general_ci NOT NULL,
   `description` varchar(45) COLLATE latin1_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -75,19 +64,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(45) COLLATE latin1_general_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=8 ;
-
---
--- Dumpning av Data i tabell `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `firstname`, `lastname`, `school`, `programme`, `email`) VALUES
-(1, 'sven', '$2y$10$GA0dL5YYgto9hNLD1ld4kOrNFG6ULTtIg6GjGx.TsrxiNa2wkm1S.', 'Sven', 'Svensson', 'Svennes verktygsmekaniska', 'qwe', 'asd@asd.sw'),
-(2, 'abba', '$2y$10$lxcGvFcmG640yj4629D35e7Uf5dwesab0IrBALxSbYJGTAso62vSe', 'sven', 'svensson', 'skolan', 'programmet', 'asd@asd.es'),
-(3, 'skatan', '$2y$10$pJ3A9LKSwDY7HK.40Y6X5ef6X45jPxB6MmZFiko2.h4djEGZy9JFe', 'alfons', 'albertsson', 'VÃ¤gga', 'NV', 'alfons@albert.se'),
-(5, 'benke', '$2y$10$m2lTjfps2wVNZq9DQ/9bue5wZHbX1NI0Nf/UtHOe.eGAxxqr54pUi', 'bengt', 'bengtsson', '', '', 'asd@asd.fr'),
-(6, 'asddc', '$2y$10$ofUVdidxgOOJzYIlTcsc0Ou9qRc2IQITdWc20g2UpkIX/7se3wGri', 'asd', 'asd', 'asd', 'Â§asd', 'asd@asd.tv'),
-(7, 'arneweisse', '$2y$10$4jVvKyylB0mFFSrSYhPC3.Wtx/StzuSCr.S6MQz2JAyVldIJY/jMi', 'arne', 'weisse', '', '', 'asd@tertad.se');
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=10 ;
 
 -- --------------------------------------------------------
 
@@ -104,17 +81,6 @@ CREATE TABLE IF NOT EXISTS `user_course` (
   KEY `course_id` (`course_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
---
--- Dumpning av Data i tabell `user_course`
---
-
-INSERT INTO `user_course` (`user_id`, `course_id`, `teacher`) VALUES
-(1, 5, 1),
-(1, 8, 1),
-(1, 9, 1),
-(1, 10, 1),
-(1, 11, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -127,10 +93,32 @@ CREATE TABLE IF NOT EXISTS `videos` (
   `description` varchar(45) COLLATE latin1_general_ci NOT NULL,
   `course_id` int(11) NOT NULL,
   `slide_id` int(11) NOT NULL,
+  `url` varchar(255) COLLATE latin1_general_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `course_id` (`course_id`),
   KEY `slide_id` (`slide_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Ersättningsstruktur för vy `view_mycourses`
+--
+CREATE TABLE IF NOT EXISTS `view_mycourses` (
+`user_id` int(11)
+,`id` int(11)
+,`name` varchar(45)
+,`code` varchar(45)
+,`description` varchar(250)
+);
+-- --------------------------------------------------------
+
+--
+-- Struktur för vy `view_mycourses`
+--
+DROP TABLE IF EXISTS `view_mycourses`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_mycourses` AS select `u`.`id` AS `user_id`,`c`.`id` AS `id`,`c`.`name` AS `name`,`c`.`code` AS `code`,`c`.`description` AS `description` from ((`users` `u` join `user_course` `b`) join `courses` `c`) where ((`b`.`user_id` = `u`.`id`) and (`b`.`course_id` = `c`.`id`));
 
 --
 -- Restriktioner för dumpade tabeller
