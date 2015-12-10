a0dc6d6afc88ef9eaa0ef6c3d4d19bd90a92f0fa
stmi11 <stefan.milakovic92@gmail.com>
2015-12-10 10:45:11 +0100

    Added "View courses" link to navigator

--- php/navigator.php
+++ php/navigator.php
@@ -6,6 +6,7 @@
 				   <li class='active'><a href='createCourse.php'>Create a Course</a></li>
 				   <li><a href='editAcc.php'>Edit Account</a></li>
 				   <li><a href='addVideo.php'>My Courses</a></li>
+				   <li><a href='viewCourses.php'>View all courses</a></li>
 				   <li><a href='php/logout.php'>Logout</a></li>
 				</ul>
 			</div>	
